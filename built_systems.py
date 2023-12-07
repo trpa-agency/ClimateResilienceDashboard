c9b7489bfef307ca7825b958d91f786dab88bd9d
Kathleen Cahya <cahya@econw.com>
2023-12-07 09:53:48 -0800

    add read file and stack bar chart function

---
+++ built_systems.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import plotly.express as px
+
+def read_file(path_file):
+    data = pd.read_csv(path_file)
+    return data
+
+def stackbar_percent(path_html, path_file, x, y, facet, color, y_title, x_title):
+    df = read_file(path_file)
+    fig = px.bar(df, x=x, y=y, color=color, barmode="stack", facet_col=facet,
+             color_discrete_sequence =["#208385","#FC9A62"])
+    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+    fig.update_layout(
+        yaxis = dict(
+            tickformat = ".0%",
+            hoverformat = ".0%",
+            title = y_title
+        ),
+        xaxis = dict(
+            title = x_title
+        )
+    )
+    fig.for_each_yaxis(lambda yaxis: yaxis.update(showticklabels=True,tickformat = ".0%"))
+    fig.update_traces(hovertemplate='Year: %{x} <br>Percentage: %{y}')
+
+    fig.write_html(path_html)
+
+stackbar_percent("/Users/cahya/Dropbox (ECONW)/kathleen/TRPA/EnergyMix.html", 
+          "/Users/cahya/Dropbox (ECONW)/kathleen/TRPA/EnergyMix_long.csv",
+          "Year","Share","Source","Type","% of Renewable Energy by Share of Total","Year")
+
