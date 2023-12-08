6214e9d23df80455f086489edfd3a121e11d5f0b
Kathleen Cahya <cahya@econw.com>
2023-12-08 14:31:43 -0800

    add grouped barchart and fix aesthetics

--- built_systems.py
+++ built_systems.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import plotly.express as px
+from pathlib import Path
 
+# Reads in csv file
 def read_file(path_file):
-    data = pd.read_csv(path_file)
+    p = Path(path_file)
+    p.expanduser()
+    data = pd.read_csv(p)
     return data
 
+# Stacked Percent Bar chart
 def stackbar_percent(path_html, path_file, x, y, facet, color, y_title, x_title):
     df = read_file(path_file)
+    config = {'displayModeBar': False}
     fig = px.bar(df, x=x, y=y, color=color, barmode="stack", facet_col=facet,
              color_discrete_sequence =["#208385","#FC9A62"])
     fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
@@ -18,14 +24,34 @@ def stackbar_percent(path_html, path_file, x, y, facet, color, y_title, x_title)
         ),
         xaxis = dict(
             title = x_title
-        )
+        ),
+        hovermode = "x"
     )
     fig.for_each_yaxis(lambda yaxis: yaxis.update(showticklabels=True,tickformat = ".0%"))
     fig.update_traces(hovertemplate='Year: %{x} <br>Percentage: %{y}')
+    
+    fig.write_html(config=config, file=path_html)
 
-    fig.write_html(path_html)
-
-stackbar_percent("/Users/cahya/Dropbox (ECONW)/25594 TRPA Climate Dashboard/Data/3.2(a)/EnergyMix.html", 
-          "/Users/cahya/Dropbox (ECONW)/25594 TRPA Climate Dashboard/Data/3.2(a)/EnergyMix_long.csv",
-          "Year","Share","Source","Type","% of Renewable Energy by Share of Total","Year")
+# Grouped Percent Bar chart
+def groupedbar_percent(path_html, path_file, x, y, facet, color, y_title, x_title):
+    df = read_file(path_file)
+    config = {'displayModeBar': False}
+    fig = px.bar(df, x=x, y=y, color=color, barmode="group", facet_col=facet,
+             color_discrete_sequence =["#208385","#FC9A62"])
+    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+    fig.update_layout(
+        yaxis = dict(
+            tickformat = ".0%",
+            hoverformat = ".0%",
+            title = y_title
+        ),
+        xaxis = dict(
+            title = x_title
+        ),
+        hovermode = "x"
+    )
+    fig.for_each_yaxis(lambda yaxis: yaxis.update(showticklabels=True,tickformat = ".0%"))
+    fig.update_traces(hovertemplate='Year: %{x} <br>Percentage: %{y}')
+    
+    fig.write_html(config=config, file=path_html)
 
