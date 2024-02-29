4bc80efdd89a8f2ad2ebd964e1a6f4d2879ca455
amcclarytrpa <amcclary@trpa.gov>
2024-02-29 14:51:25 -0800

    Updating to handle new Mode share data

--- built_systems.py
+++ built_systems.py
@@ -390,7 +390,8 @@ def get_data_mode_share():
     #Calculate percentage
     modeshare_data_grouped['Percentage'] = (modeshare_data_grouped['Number'] / modeshare_data_grouped['Total']) * 100
 
-    modeshare_data_grouped['Season'] = pd.Categorical(modeshare_data_grouped['Season'], ['Winter', 'Spring', 'All', 'Summer', 'Fall'])
+    modeshare_data_grouped['Season'] = pd.Categorical(modeshare_data_grouped['Season'], 
+                                                      ['Winter', 'Q1', 'Spring', 'Q3', 'Summer', 'Fall'])
     modeshare_data_grouped = modeshare_data_grouped.sort_values(by=['Year', 'Season'])
     #Order year season so that it graphs correctly
     modeshare_data_grouped['Year_Season'] = pd.Categorical(modeshare_data_grouped['Year_Season'], modeshare_data_grouped['Year_Season'].unique())
